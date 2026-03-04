83e3a8beac8ca9e474ea644a93bc12ab31adc7c2
vanmobe <ben.vanmol@co-lab.eu>
2026-03-04 12:05:46 +0100

    Reduce Windows header pollution in wing_osc translation unit

--- src/core/wing_osc.cpp
+++ src/core/wing_osc.cpp
@@ -28,6 +28,12 @@
 #include <cstring>
 
 #if defined(_WIN32)
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
